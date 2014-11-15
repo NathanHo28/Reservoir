f63c95b148f6af8cefc120090743b4e26812f0db
NATHAN <nathan.ho28@gmail.com>
2014-11-15 14:04:32 -0500

    corrected reservation create action to include the current_user's user_id when saved

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,26 @@
 class ReservationsController < ApplicationController
   before_filter :load_restaurant
+  # before_filter :ensure_logged_in
 
   def show
   	@reservation = Reservation.find(params[:id])
+
+    if current_user
+    @reservation = @Restaurant.reviews.build
+    end
   end
 
   def create
   	# @reservation = Reservation.new
-   #  @reservation.user = current_user
-      @restaurant = Restaurant.find(params[:restaurant_id])
-      @reservation = @restaurant.reservations.build(reservation_params)
-
+      # @reservation.user = current_user
+      # @restaurant = Restaurant.find(params[:restaurant_id])
+      # @reservation = @restaurant.reservations.build(reservation_params)
+      @reservation = Reservation.new(reservation_params)
+      @reservation.user_id = current_user.id
+      @reservation.restaurant_id = params[:restaurant_id]
+  
   		if @reservation.save
-  			redirect_to restaurants_path notice: "Congratulations! We looks forward to seeing you =<(^o^<)"
+  			redirect_to restaurants_path, notice: "Congratulations! We looks forward to seeing you =<(^o^<)"
   		else
   			render 'restaurants/show'
   		end
