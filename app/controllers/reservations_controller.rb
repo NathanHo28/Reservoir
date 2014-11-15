1be699b37e2a9fff1194942fd94826caffcbe291
NATHAN <nathan.ho28@gmail.com>
2014-11-15 10:13:20 -0500

    added reservation function working on the restaurant show page. user_id not saving with reservation(NEED TO FIX)

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,20 @@
 class ReservationsController < ApplicationController
+  before_filter :load_restaurant
+
   def show
   	@reservation = Reservation.find(params[:id])
   end
 
   def create
-  	@reservation = Reservation.new
+  	# @reservation = Reservation.new
+   #  @reservation.user = current_user
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @reservation = @restaurant.reservations.build(reservation_params)
+
   		if @reservation.save
-  			redirect_to restaurant_url notice: "Congratulations! We looks forward to seeing you =<(^o^<)"
+  			redirect_to restaurants_path notice: "Congratulations! We looks forward to seeing you =<(^o^<)"
   		else
-  			render :new
+  			render 'restaurants/show'
   		end
   end
 
@@ -19,11 +25,14 @@ class ReservationsController < ApplicationController
   def destroy
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
-    redirect_to restaurants_url
   end
 
   private
   def reservation_params
-    params.require(:reservation).permit(:time, :party_size, :restaurant_id, :name, :email, :request)
+    params.require(:reservation).permit(:time, :party_size, :restaurant_id, :user_id, :name, :email, :request)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
